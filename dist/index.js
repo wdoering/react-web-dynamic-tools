ae65fbcae34e49b1a34f3587962254c7931d7855
brunoteixeirasilva <bruno.teixeira.silva@gmail.com>
2019-12-30 19:38:40 -0300

    UPDATE: bugfix on view.js; version bump

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback as useCallback$1 } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { Button, Typography, makeStyles as makeStyles$1, ListItem, ListItemSecondaryAction, FormLabel, TextField, InputAdornment, Paper, List, FormControlLabel, Checkbox, Card, CardContent, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelActions, ExpansionPanelDetails, Dialog as Dialog$1, DialogTitle as DialogTitle$1, DialogContent as DialogContent$1, DialogActions as DialogActions$1, Chip } from '@material-ui/core';
@@ -1520,7 +1520,7 @@ var DynamicForm = function DynamicForm(_ref3) {
       _useState10 = _slicedToArray(_useState9, 2),
       errors = _useState10[0],
       setErrors = _useState10[1],
-      oService = useCallback$1(model.getService(firebase), [model, firebase]);
+      oService = useCallback(model.getService(firebase), [model, firebase]);
 
   useEffect(function () {
     if (id && (!model.uid || model.uid !== id)) {
@@ -1535,7 +1535,7 @@ var DynamicForm = function DynamicForm(_ref3) {
    * @param {string} prop
    */
 
-  var handleChange = useCallback$1(function (prop, value) {
+  var handleChange = useCallback(function (prop, value) {
     var v = _objectSpread2({}, values, _defineProperty({}, prop, value));
 
     setValues(v);
@@ -1547,7 +1547,7 @@ var DynamicForm = function DynamicForm(_ref3) {
    * @param {any} value
    */
 
-  var validate = useCallback$1(function (prop, value) {
+  var validate = useCallback(function (prop, value) {
     clearTimeout(validateTimeout);
     model[prop] = value;
     validateTimeout = setTimeout(function () {
@@ -1560,7 +1560,7 @@ var DynamicForm = function DynamicForm(_ref3) {
    * saves model at the server, after validation
    */
 
-  var save = useCallback$1(function () {
+  var save = useCallback(function () {
     model.$fill(values);
     var validation = model.$validate();
     console.log("validation", validation);
@@ -1710,7 +1710,7 @@ var createFilters = function createFilters(model, i18n, updateFilters) {
       values = _useState4[0],
       setValues = _useState4[1];
 
-  var handleChange = useCallback$1(function (property, value) {
+  var handleChange = useCallback(function (property, value) {
     var v = _objectSpread2({}, values, _defineProperty({}, property, value));
 
     setValues(v);
@@ -2154,7 +2154,7 @@ var DynamicView = function DynamicView(_ref3) {
       setValues = _useState4[1],
       history = useHistory(),
       deleteConfirmationDialogRef = React.createRef(),
-      oService = useCallback$1(model.getService(firebase), [model, firebase]);
+      oService = useCallback(model.getService(firebase), [model, firebase]);
 
   useEffect(function () {
     //TODO: implement service flexibility
@@ -2165,7 +2165,7 @@ var DynamicView = function DynamicView(_ref3) {
       });
     }
   }, [model, id, oService, setValues]);
-  var remove = useCallback$1(function () {
+  var remove = useCallback(function () {
     oService.patch(values.uid, {
       deleted: true
     });
--- src/dynamic/_hooks.js
+++ src/dynamic/_hooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { getServiceList, typeShouldUseService } from './_functions';
 
 /**
